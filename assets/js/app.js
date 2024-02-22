63e0f1184eefc1a7637b5bc404e2e88c930f50d0
Bruno Cabado <sr.brunocabado@gmail.com>
2024-02-22 16:30:16 +0100

    add new social media

--- assets/js/app.js
+++ assets/js/app.js
@@ -228,8 +228,7 @@ let footer = $(`
     </a>
 
 
-    <!--
-    <a class="social-button instagram" href="" target="_blank" >
+    <a class="social-button instagram" href="https://www.instagram.com/brunocabado/" target="_blank" >
       <svg class="instagram-icon-footer" x="0px" y="0px" viewBox="0 0 202.5 202.5"
         style="enable-background:new 0 0 202.5 202.5;">
         <circle id="littleCircle" class="st0" cx="101" cy="101.5" r="18.9" />
@@ -238,7 +237,6 @@ let footer = $(`
         <path id="camera" class="st0" d="M79,60.5h44c10.5,0,19,8.5,19,19v44c0,10.5-8.5,19-19,19H79c-10.5,0-19-8.5-19-19v-44                                                                            C60,69,68.5,60.5,79,60.5z" />
       </svg>
     </a>
-    -->
 
     <a class="social-button github" href="https://github.com/Kr4is" target="_blank">
     <!-- SVG code for Github icon -->
